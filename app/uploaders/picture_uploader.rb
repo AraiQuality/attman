7a78898505a4b828c8c8525c82400f16ef699ea9
Arai <arai.quality@gmail.com>
2018-11-17 08:15:53 +0000

    Add user microposts

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,14 +1,16 @@
 class PictureUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
+  # アップロードファイルの保存先ディレクトリは上書き可能
+  # 下記はデフォルトの保存先
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -33,11 +35,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   process resize_to_fit: [50, 50]
   # end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  # アップロード可能な拡張子のリスト
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
